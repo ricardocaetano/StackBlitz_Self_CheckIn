00ed8bc7aecc5cf50c6f93e51016eef9eee9bc8a
Pedro Silva <psilva@opensoft.pt>
2019-05-03 12:14:54 +0100

    Version Update 4.0.0-beta.c5087706

---
+++ checkpoint6/cp6-end/CONTRIBUTING.md
@@ -0,0 +1,6 @@
+## README Links in New Tabs
+
+Since the `README` is to be placed inside an iframe, if any `README` links are to be forcefully opened in new tabs, they should be encased in `HTML` tags, with `target="_blank"`.
+
+Example:
+`<a href="https://www.examples.com" target="_blank">Example Link Text</a>`
---
+++ checkpoint6/cp6-end/src/app/components/guests/components/guest/other-services/other-services.en-US.ts
@@ -0,0 +1,40 @@
+import{LfI18n} from "@lightweightform/core";
+import { I18N_NUMBER_THOUSANDS_SEPARATOR_KEY, I18N_VALUE_LABEL_KEY } from "@lightweightform/bootstrap-theme";
+
+export const OTHER_SERVICES_I18N_EN_US = LfI18n.mergeTranslations({
+    '/guests/?/otherServices':{
+        [I18N_VALUE_LABEL_KEY]: "Other Services",
+    },
+
+    '/guests/?/otherServices/h1Radio':{
+        [I18N_VALUE_LABEL_KEY]: 'Laundry Service?',
+        options : [
+            {label: 'Yes', value: 'Y'},
+            {label: 'No', value: 'N'}
+        ],
+    }, 
+
+    '/guests/?/otherServices/h2Radio':{
+        [I18N_VALUE_LABEL_KEY]: 'Gym Service?',
+        options : [
+            {label: 'Yes', value: 'Y'},
+            {label: 'No', value: 'N'}
+        ],
+    }, 
+
+    '/guests/?/otherServices/h3Number':{
+        [I18N_NUMBER_THOUSANDS_SEPARATOR_KEY] : "",
+        [I18N_VALUE_LABEL_KEY]: 'Days With Personal Trainer',
+    }, 
+
+    '/guests/?/otherServices/h4ComputedNumber':{
+        [I18N_VALUE_LABEL_KEY]: 'Total Price',
+        isDisabled : true,
+        legend : "Total Price",
+        decimalSeparator : ",",
+        [I18N_NUMBER_THOUSANDS_SEPARATOR_KEY] : ".",
+        suffix : " $",
+    }, 
+    
+});
+
