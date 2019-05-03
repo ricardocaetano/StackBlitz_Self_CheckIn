f7db1861914387e0c55e5bbd3f3d98db953d82df
Pedro Silva <psilva@opensoft.pt>
2019-05-03 12:24:21 +0100

    Version Update 4.0.0-beta.c5087706

--- checkpoint5/cp5-begin/src/main.ts
+++ checkpoint5/cp5-begin/src/main.ts
@@ -3,9 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { configure } from 'mobx';
-
-configure({ isolateGlobalState: true });
 
 if (environment.production) {
   enableProdMode();
